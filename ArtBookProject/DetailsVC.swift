781bcac6fceac5d533514c2853a6db49b6eb9aee
Mert can Ünek <mrtcnunk@gmail.com>
2023-06-14 14:44:39 +0300

    update

--- ArtBookProject/DetailsVC.swift
+++ ArtBookProject/DetailsVC.swift
@@ -7,28 +7,51 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var imageView: UIImageView!
-    
-    
     @IBOutlet weak var nameText: UITextField!
-    
-    
     @IBOutlet weak var artistText: UITextField!
-    
     @IBOutlet weak var yearText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        //Recognizers
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        
+        view.addGestureRecognizer(gestureRecognizer)
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
     }
     
 
     @IBAction func saveButton(_ sender: Any) {
     }
     
+    @objc func selectImage(){
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+                    
+    @objc func hideKeyboard(){
+        
+        view.endEditing(true)
+    }
 
 }
